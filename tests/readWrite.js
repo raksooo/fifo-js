40c4cb21693444f674f88446d3ce3dc81414e1fe
Oskar Nyberg <oskar@oskarnyberg.com>
2016-07-27 18:28:42 +0200

    Added more tests.

--- tests/readWrite.js
+++ tests/readWrite.js
@@ -1,29 +1,42 @@
-const FIFO = require('../fifo')
+const expect = require('chai').expect,
+      FIFO = require('../fifo')
 
-let fifo = new FIFO('asdf.fifo')
-fifo.write('1\n', () => {
-    console.log('in: 1')
+describe('Read and write', function() {
+    let fifo
+
+    before(function() {
+        fifo = new FIFO()
+    })
+
+    after(function() {
+        fifo.close()
+    })
+
+    it('should write and read asynchronously', function(done) {
+        let string = 'Test string 1'
+        fifo.write(string)
+        fifo.read(data => {
+            expect(data).to.equal(string)
+            done()
+        })
+    })
+
+    it('should read and write asynchronously', function(done) {
+        let string = 'Test string 2'
+        fifo.read(data => {
+            expect(data).to.equal(string)
+            done()
+        })
+        fifo.write(string)
+    })
+
+    it('should read asyncronously and write synchronously', function(done) {
+        let string = 'Test string 3'
+        fifo.read(data => {
+            expect(data).to.equal(string)
+            done()
+        })
+        fifo.writeSync(string)
+    })
 })
-fifo.read((data) => {
-    console.log('out: ', data)
-})
 
-setTimeout(() => {
-    fifo.read((data) => {
-        console.log('out: ', data)
-    })
-    fifo.write('2\n', () => {
-        console.log('in: 2')
-    })
-}, 100)
-
-setTimeout(() => {
-    fifo.read((data) => {
-        console.log('out: ', data)
-    })
-    console.log(fifo.writeSync('4\n'))
-}, 400)
-
-setTimeout(() => {
-    fifo.close()
-}, 500)

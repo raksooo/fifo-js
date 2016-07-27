905c907951f29f7c079c92142f4a323f3c6c02e8
Oskar Nyberg <oskar@oskarnyberg.com>
2016-07-28 00:52:31 +0200

    Moved around methods to have public ones at the top.

--- fifo.js
+++ fifo.js
@@ -10,6 +10,63 @@ class FIFO {
         this._open = true
     }
 
+    get open() {
+        return this._open && this._pathExists()
+    }
+
+    read(callback) {
+        this._throwIfClosed()
+
+        fs.readFile(this.path, (err, data) => {
+            if (this.open && callback) {
+                callback(data.toString().trim())
+            }
+        })
+    }
+
+    /**
+     * Warning: Doesn't work in combination with fifo.write() since this locks
+     * up the node process.
+     */
+    readSync() {
+        this._throwIfClosed()
+
+        return fs.readFileSync(this.path).toString().trim()
+    }
+
+    setReader(reader) {
+        this._throwIfClosed()
+
+        this.reader = reader
+        this._reader()
+    }
+
+    write(string, callback) {
+        this._throwIfClosed()
+
+        let writer = fs.createWriteStream(this.path, { autoClose: true })
+        writer.end(string, () => {
+            if (this.open && callback) {
+                callback()
+            }
+        })
+    }
+
+    writeSync(string) {
+        this._throwIfClosed()
+
+        let child = cp.execSync('echo "' + string + '" > ' + this.path)
+        return string
+    }
+
+    close() {
+        this._open = false
+
+        if (!this.preserve) {
+            this._unlink()
+        }
+    }
+
     _initFifo() {
         let stat = this._pathExists()
         if (!stat) {
@@ -45,13 +102,6 @@ class FIFO {
         return path
     }
 
-    setReader(reader) {
-        this._throwIfClosed()
-
-        this.reader = reader
-        this._reader()
-    }
-
     _reader() {
         fs.readFile(this.path, (err, data) => {
             if (this.open) {
@@ -59,52 +109,6 @@ class FIFO {
                 this._reader()
             }
         })
-    }
-
-    read(callback) {
-        this._throwIfClosed()
-
-        fs.readFile(this.path, (err, data) => {
-            if (this.open && callback) {
-                callback(data.toString().trim())
-            }
-        })
-    }
-
-    /**
-     * Warning: Doesn't work in combination with fifo.write() since this locks
-     * up the node process.
-     */
-    readSync() {
-        this._throwIfClosed()
-
-        return fs.readFileSync(this.path).toString().trim()
-    }
-
-    write(string, callback) {
-        this._throwIfClosed()
-
-        let writer = fs.createWriteStream(this.path, { autoClose: true })
-        writer.end(string, () => {
-            if (this.open && callback) {
-                callback()
-            }
-        })
-    }
-
-    writeSync(string) {
-        this._throwIfClosed()
-
-        let child = cp.execSync('echo "' + string + '" > ' + this.path)
-        return string
-    }
-
-    close() {
-        this._open = false
-
-        if (!this.preserve) {
-            this._unlink()
-        }
     }
 
     _unlink() {
@@ -117,10 +121,6 @@ class FIFO {
         if (!this.open) {
             throw new FIFOError(this.path, "Fifo isn't open.")
         }
-    }
-
-    get open() {
-        return this._open && this._pathExists()
     }
 }
 

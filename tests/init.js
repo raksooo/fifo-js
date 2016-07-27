d2778937a7b3574965a10e5b9f7eb6dfa5db8ed9
Oskar Nyberg <oskar@oskarnyberg.com>
2016-07-27 17:58:42 +0200

    Improved some test cases with mocha.

---
+++ tests/init.js
@@ -0,0 +1,34 @@
+const expect = require('chai').expect,
+      FIFO = require('../fifo'),
+      FifoException = require('../fifoException'),
+      fs = require('fs')
+
+describe('Initialization', function() {
+    let fifo
+    let exceptionPath
+
+    before(function() {
+        fifo = new FIFO()
+    })
+
+    after(function() {
+        fs.unlink(exceptionPath)
+    })
+
+    it('should exist a fifo at the objects path', function() {
+        let stat = fs.statSync(fifo.path)
+        expect(stat.isFIFO()).to.be.true
+    })
+
+    it('should not exist a fifo at the objects path', function() {
+        fifo.close()
+        expect(fs.statSync.bind(fs, fifo.path)).to.throw(Error)
+    })
+
+    it('should throw exception since non-fifo file exists', function() {
+        exceptionPath = 'exceptionTest.fifo'
+        fs.closeSync(fs.openSync(exceptionPath, 'w'))
+        expect(() => new FIFO(exceptionPath)).to.throw(Error)
+    })
+})
+
